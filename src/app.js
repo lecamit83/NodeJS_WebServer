e1db97cd590462896fed34a4a4b1e422be56768d
Le Cam <thantoandoc83@gmail.com>
2019-04-08 17:02:42 +0700

    Customize views folder

--- src/app.js
+++ src/app.js
@@ -2,7 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+
 app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, '../templates'));
+
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/', (req, res)=>{
